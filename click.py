c0f7a57c48ae02a3dfade26e9b8583bf575aa499
Wizardwa <ewaatdavis@gmail.com>
2023-12-06 01:27:14 +0300

    position distribution

---
+++ click.py
@@ -0,0 +1,36 @@
+#!/bin/python3 
+
+import pyautogui
+import os
+import sys
+import time
+
+#position of Software Updater x=20, y=318
+
+position = pyautogui.moveTo(x=20, y=318)
+
+#show current position
+current_post = pyautogui.position()
+print(current_post)
+
+#click event
+
+click = pyautogui.click()
+
+#move to the next position x=960 y=487
+time.sleep(18)
+
+position_ok = pyautogui.moveTo(x=960, y=487)
+click = pyautogui.click()
+
+#write something on the screen
+
+keyboard = pyautogui.typewrite("This is good\n")
+time.sleep(2)
+
+try:
+    keyboard = pyautogui.hotkey('ctrl', 'c')
+    print('\n')
+   #os.system('clear')
+except KeyboardInterrupt:
+    os.system('clear')
